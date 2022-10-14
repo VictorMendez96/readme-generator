ee30c97d620a7632c1d2dfe515c5cfccbffe3112
V Mendez <victormendez990@gmail.com>
2022-10-13 22:37:47 -0600

    Finished function to print results

--- Develop/index.js
+++ Develop/index.js
@@ -3,15 +3,16 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // Include js to generate the license badge
-const markdown = require('./utils/generateMarkdown')
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = ["Enter the name of your project:", "Describe your Project:", "How do you install your application?", "How do you use your application?", "How can users contribute to your application?", "How do you test your application?", "Choose your license:", "Enter your Github username:", "Enter your email address:"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-
-}
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Readme Created!'))
+};
 
 // TODO: Create a function to initialize app
 function init() {
@@ -64,6 +65,10 @@ function init() {
                 message: questions[8],
             },
         ])
+        .then(function (response) {
+            const readme = markdown.generateMarkdown(response);
+            writeToFile('', readme);
+        })
 };
 
 // Function call to initialize app
