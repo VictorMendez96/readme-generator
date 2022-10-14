90b08c265e8b062b66ce0b2e74e31bc39d73d09e
V Mendez <victormendez990@gmail.com>
2022-10-14 00:22:47 -0600

    Final adjustments

---
+++ Develop/assets/ReadMe.md
@@ -0,0 +1,35 @@
+# Test 
+
+  ![badge](https://shields.io/badge/license-GNU LGPLv3-blue)
+
+  ## Table of Contents
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+  - [License](#License)
+
+  ## Description
+  Test
+
+  ## Installation
+  Test
+
+  ## Usage
+  Test
+
+  ## Contributing
+  Test
+
+  ## Tests
+  Test
+
+  ## Questions
+  [GitHub](https://github.com/Testhub)
+
+  ## License
+    GNU LGPLv3
+    
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,41 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (license) {
-    return `![badge](https://shields.io/badge/license-${license}-blue)`
-  } else {
+  if (license = 'None') {
     return ``
+  } else {
+    license = license.split(' ').join('')
+    return `![badge](https://shields.io/badge/license-${license}-blue)`
   }
 } 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license) {
-    return `- [License](#License)`
-  } else {
+  if (license = 'None') {
     return ``
+  } else {
+    return `- [License](#License)`
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license) {
+  if (license = 'None') {
+    return ``
+  } else {
     return `## License
     ${license}
     `
-  } else {
-    return ``
   }
-  
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  // renderLicenseBadge(data.license)
-  // renderLicenseLink(data.license)
-  // renderLicenseSection(data.license)
   return `# ${data.title}
 
   ${renderLicenseBadge(data.license)}
