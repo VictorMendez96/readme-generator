73de28166ad1aed1a4a97204f417ca6985a2d2de
V Mendez <victormendez990@gmail.com>
2022-10-13 21:43:57 -0600

    Setup template for prompts

--- Develop/index.js
+++ Develop/index.js
@@ -5,16 +5,72 @@ const fs = require('fs');
 // Include js to generate the license badge
 const markdown = require('./utils/generateMarkdown')
 
+// Add bottom bar for user updates 
+
 // TODO: Create an array of questions for user input
 const questions = ["Enter the name of your project:", "Describe your Project:", "How do you install your application?", "How do you use your application?", "How can users contribute to your application?", "How do you test your application?", "Choose your license:", "Enter your Github username:", "Enter your email address:"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+            {
+                type: ,
+                name: ,
+                message: ,
+            },
+        ])
+}
 
 // Function call to initialize app
 init();
