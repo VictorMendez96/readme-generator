430a1d8c2f457d4304340f7aa2c6f78bc9695a51
V Mendez <victormendez990@gmail.com>
2022-10-13 23:22:29 -0600

    Add to Readme format

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,32 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  badge = 'test'
-  return badge
+  if (license) {
+    return badge
+  } else {
+    return ""
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return link
+  if (license) {
+    return link
+  } else {
+    return ""
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return section
+  if (license) {
+    return section
+  } else {
+    return ""
+  }
+  
 }
 
 // TODO: Create a function to generate markdown for README
@@ -27,31 +39,33 @@ function generateMarkdown(data) {
   ${badge}
 
   ## Table of Contents
-  - [Description]()
-  - [Installation]()
-  - [Usage]()
-  - [Contributing]()
-  - [Tests]()
-  - [Questions]()
-  - [License]()
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+  - [License](#License)
 
   ## Description
-
+  ${data.description}
 
   ## Installation
-
+  ${data.installation}
 
   ## Usage
-
+  ${data.usage}
 
   ## Contributing
-
+  ${data.contribution}
 
   ## Tests
-
+  ${data.test}
 
   ## Questions
   [GitHub](https://github.com/${data.github})
+
+
 
 
   ## License
